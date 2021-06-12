8491856e4f1605404fd7ecc5be1f10a5565aba5c
91781 <91781@DESKTOP-UM01V88>
2021-06-12 12:30:20 +0530

    updating the code to Firstclass.java

--- Testing/src/FirstClass.java
+++ Testing/src/FirstClass.java
@@ -3,7 +3,9 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.out.println(12345);
+System.out.println("hello hi");
 	}
 
 }
